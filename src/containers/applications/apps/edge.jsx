37b464149eef01e05603ab293e102dc0ac494e91
CoolBeans <147670938+cocoBEAMS@users.noreply.github.com>
2024-04-18 17:25:42 -0700

    Update edge.jsx

--- src/containers/applications/apps/edge.jsx
+++ src/containers/applications/apps/edge.jsx
@@ -4,7 +4,7 @@ import { Icon, ToolBar, LazyComponent } from "../../../utils/general";
 
 export const EdgeMenu = () => {
   const wnapp = useSelector((state) => state.apps.edge);
-  const [url, setUrl] = useState("https://www.google.com/?igu=1");
+  const [url, setUrl] = useState("https://classoco.vercel.app");
   const [ierror, setErr] = useState(true);
   const [isTyping, setTyping] = useState(false);
   const [hist, setHist] = useState(["https://bing.com", "https://bing.com"]);
